02918e7f42152657f33dbcb9fcb260f53cae9375
wealthoceaninstitute-commits <wealthoceaninstitute@gmail.com>
2025-12-10 23:09:52 +0530

    Update MultiBroker_Router.py

--- MultiBroker_Router.py
+++ MultiBroker_Router.py
@@ -53,7 +53,7 @@ app.add_middleware(
     allow_origins=[
         "https://multibrokertrader-production.up.railway.app",
         "https://multibroker-trader.onrender.com",
-        "https://multibrokertrader-production-b4e2.up.railway.app"
+        "https://multibrokertrader-production-b4e2.up.railway.app",
         "https://multibrokertradermultiuser-production-f735.up.railway.app/"
     ],
     allow_credentials=True,
@@ -1981,6 +1981,7 @@ def route_modify_order(payload: Dict[str, Any] = Body(...)):
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("MultiBroker_Router:app", host="127.0.0.1", port=5001, reload=False)
+
 
 
 
